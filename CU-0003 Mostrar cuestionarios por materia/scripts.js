8a7a1747faa9f8721b3440b816f0460ef52eeb5d
diegoam11 <diego.alvarez2@unmsm.edu.pe>
2023-06-08 23:18:43 -0500

    Se hizo el enlace de las distintas páginas del proyecto

--- CU-0003 Mostrar cuestionarios por materia/scripts.js
+++ CU-0003 Mostrar cuestionarios por materia/scripts.js
@@ -2,3 +2,10 @@ function toggleSidebar() {
     let sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('open');
 }
+
+function goToMainMenu() {
+    window.location.href = '../CU-0004 Habilitar cuestionario general/home.html';
+}
+
+const mainMenuButton = document.getElementById('back-button')
+mainMenuButton.addEventListener("click", () => goToMainMenu())
--- CU-0004 Habilitar cuestionario general/scripts.js
+++ CU-0004 Habilitar cuestionario general/scripts.js
@@ -2,3 +2,11 @@ function toggleSidebar() {
     let sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('open');
 }
+
+function goToQuestionare() {
+    window.location.href = '../CU-0003 Mostrar cuestionarios por materia/questionare.html';
+}
+
+const buttonMath = document.getElementById("math-button");
+buttonMath.addEventListener("click", () => goToQuestionare())
+
